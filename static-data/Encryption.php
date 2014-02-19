4e89b11c9e3f6675e96fdba08e471cf31a83cdf1
Kouki Kuriyama <Kouki@Kouki-no-MacBook-Pro.local>
2014-02-19 18:16:40 +0900

    jlab-script-plus Ver0.01b

--- static-data/Encryption.php
+++ static-data/Encryption.php
@@ -2,7 +2,7 @@
 
 function EncInit($EncKey){
 
-	$GLOBALS["Newtd"] = mcrypt_module_open('blowfish', '', 'ecb', '');
+	$GLOBALS["Newtd"] = mcrypt_module_open('des', '', 'ecb', '');
 	$GLOBALS["EncKey"] = substr($EncKey, 0, mcrypt_enc_get_key_size($GLOBALS["Newtd"]));
 	$GLOBALS["IV"]  = mcrypt_create_iv(mcrypt_enc_get_iv_size($GLOBALS["Newtd"]), MCRYPT_RAND);
 	mcrypt_generic_init($GLOBALS["Newtd"],$GLOBALS["EncKey"] ,$GLOBALS["IV"]);
